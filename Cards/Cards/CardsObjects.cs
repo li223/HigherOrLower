26c4775c03a5b83241fdb81d9d938e55eed75b93
li223 <li223@users.noreply.github.com>
2018-01-11 14:21:28 +0000

    I'm a mess xd

---
+++ Cards/Cards/CardsObjects.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cards.Objects
+{
+    public sealed class Card
+    {
+        public string CardID { get; set; }
+        public string CardSuit { get; set; }
+        public int CardValue { get; set; }
+    }
+    public sealed class CardDeck
+    {
+        private List<Card> Cards = new List<Card>();
+        private char TryGetItem(int index, out bool result)
+        {
+            result = true;
+            switch (index)
+            {
+                case 0: return 'h';
+                case 1: return 'd';
+                case 2: return 's';
+                case 3: return 'c';
+                default:
+                    result = false;
+                    throw new ArgumentException("Given index was not found");
+            }
+        }
+        public List<Card> GenerateDeck()
+        {
+            var max = 52;
+            var suitnumber = 13;
+            var current_suit = 0;
+            var current_card_number = 1;
+            while (true)
+            {
+                if (current_card_number < 10)
+                {
+                    if (current_suit > 3)
+                        break;
+                    Cards.Add(new Card()
+                    {
+                        CardID = $"c0{current_card_number}{TryGetItem(current_suit, out bool result)}",
+                        CardValue = current_card_number,
+                        CardSuit = TryGetItem(current_suit, out bool suitresult).ToString()
+                    });
+                    max--;
+                    suitnumber--;
+                    current_card_number++;
+                }
+                else
+                {
+                    Cards.Add(new Card()
+                    {
+                        CardID = $"c{current_card_number}{TryGetItem(current_suit, out bool result)}",
+                        CardValue = current_card_number,
+                        CardSuit = TryGetItem(current_suit, out bool suitresult).ToString()
+                    });
+                    max--;
+                    suitnumber--;
+                    current_card_number++;
+                }
+                if(suitnumber == 0)
+                {
+                    current_suit += 1;
+                    current_card_number = 1;
+                    suitnumber = 13;
+                }
+                if (max == 0) break;
+            }
+            return Cards;
+        }
+    }
+}
---
+++ Cards/Cards/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Cards.Objects;
+using System.IO;
+using System.Windows.Threading;
+
+namespace Cards
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int Score = 0;
+        Random rand = new Random();
+        List<Card> CardDeck { get; set; }
+        Card NextCard { get; set; }
+        Card CurrentCard { get; set; }
+        DispatcherTimer timer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(5);
+            timer.Start();
+            timer.Tick += Timer_Tick;
+        }
+
+        private async void Timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            if (NextCard.CardValue > CurrentCard.CardValue)
+            {
+                if (HigherButton.IsChecked.Value)
+                {
+                    MessageBox.Show("Correct");
+                    Score += 1;
+                }
+            }
+            else if (LowerButton.IsChecked.Value)
+            {
+                MessageBox.Show("Correct");
+                Score += 1;
+            }
+            else
+                MessageBox.Show("Incorrect");
+            timer.Start();
+            await LoadNextCard();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CardDeck = new CardDeck().GenerateDeck();
+            NextCard = CardDeck[rand.Next(0, CardDeck.Count)];
+            CurrentCard = CardDeck[rand.Next(0, CardDeck.Count)];
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "Cards", $"{CurrentCard.CardID}.png");
+            CardHolder.Source = new BitmapImage(new Uri(path));
+        }
+        public async Task LoadNextCard()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "Cards", $"{NextCard.CardID}.png");
+            CurrentCard = NextCard;
+            await Task.Delay(4);
+            CardHolder.Source = new BitmapImage(new Uri(path));
+            NextCard = CardDeck[rand.Next(0, CardDeck.Count)];
+        }
+    }
+}
